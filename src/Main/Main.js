7085b8ead4757731593f0593d1010af3463f2092
Raihan <aboraihan14@gmail.com>
2024-11-22 16:37:56 +0600

    React Router Tailwind setup

---
+++ src/Main/Main.js
@@ -0,0 +1,21 @@
+import { Outlet } from "react-router-dom"
+
+const Main = () => {
+  return (
+    <div>
+
+      {/* Header  */}
+
+
+      {/* Dynamic Sections  */}
+      <Outlet />
+
+
+
+      {/* Footer  */}
+
+    </div>
+  )
+}
+
+export default Main
