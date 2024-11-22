10a014d6f0424240b9a69dbb3c1c95a8e79db2bf
Rohan <rohan26ir@gmail.com>
2024-11-22 19:53:17 +0600

    Add basic footer

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom"
 import Navbar from "../Components/Navbar/Navbar"
+import Footer from "../Components/Footer/Footer"
 
 const MainLayout = () => {
     return (
@@ -16,7 +17,7 @@ const MainLayout = () => {
                 <Outlet />
             </main>
 
-            <footer></footer>
+            <Footer></Footer>
 
         </div>
     )
