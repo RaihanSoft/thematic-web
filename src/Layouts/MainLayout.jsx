2805da2ac1eb8b656e1ce1ce054607ecced87776
Ahsan-1015 <ahshanh964@gmail.com>
2024-11-22 20:10:59 +0600

    banner

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
 import { Outlet } from 'react-router-dom';
 import Navbar from '../Components/Navbar/Navbar';
 import BannerSlide from '../Components/BannerSlide/BannerSlide';
-=======
-import { Outlet } from "react-router-dom"
-import Navbar from "../Components/Navbar/Navbar"
-import Footer from "../Components/Footer/Footer"
->>>>>>> 10a014d6f0424240b9a69dbb3c1c95a8e79db2bf
 
 const MainLayout = () => {
   return (
@@ -26,18 +20,4 @@ const MainLayout = () => {
   );
 };
 
-<<<<<<< HEAD
 export default MainLayout;
-=======
-            <main>
-                <Outlet />
-            </main>
-
-            <Footer></Footer>
-
-        </div>
-    )
-}
-
-export default MainLayout
->>>>>>> 10a014d6f0424240b9a69dbb3c1c95a8e79db2bf
